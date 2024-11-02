04fca4a8356e4ed23fb6cc0a0706a83f2877bde7
aaaakash001 <aaaakash001@gmail.com>
2024-11-02 06:03:26 +0530

    update_demo

--- ner_demo.py
+++ ner_demo.py
@@ -14,7 +14,7 @@ nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
-
+nltk.download('punkt_tab')
 
 tag_colors = {
     1: "#8FBC8F",  # Darker Green

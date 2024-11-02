c16a42b33280f4556983f8a56208a2de6758f436
aaaakash001 <aaaakash001@gmail.com>
2024-11-02 06:08:15 +0530

    Update ner_demo.py

--- ner_demo.py
+++ ner_demo.py
@@ -15,6 +15,7 @@ nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
 nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
 
 tag_colors = {
     1: "#8FBC8F",  # Darker Green
